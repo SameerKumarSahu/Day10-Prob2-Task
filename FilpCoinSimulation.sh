7a08a53d2aec54a68d798e42b484693fdf0aee56
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-26 06:27:39 +0530

    Loop through Multiple Times

--- FilpCoinSimulation.sh
+++ FilpCoinSimulation.sh
@@ -1,14 +1,33 @@
 #!/bin/bash -x
 echo "Welcome to Filp Coin Simulation"
 
+Heads=0
+Tails=0
+for (( i=0; i<=20;i++))
+do
 
-x=$((RANDOM%10))
+	x=$((RANDOM%10))
         FLIP=$(( x % 2 ))
 
                 if [ $FLIP -eq 0 ]
 
                 then
-                        echo " Head come "
+                         ((Heads++))
                 else
-                        echo " Tail come "
+			 ((Tails++))
                 fi
+
+done
+
+echo "Number of time Heads are" $Heads
+echo "Number of time Tails are" $Tails
+
+if [[ $Heads -gt $Tails ]]
+then
+        echo "Heads Win"
+elif [[ $Heads -lt $Tails ]]
+then
+        echo "Tails Win"
+else
+        echo "Tie"
+fi
