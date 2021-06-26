6f0c6716299c072b53f85eb826a975b14b973a3a
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-26 06:19:59 +0530

    Display Head or Tail

--- FilpCoinSimulation.sh
+++ FilpCoinSimulation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 echo "Welcome to Filp Coin Simulation"
 
+
+x=$((RANDOM%10))
+        FLIP=$(( x % 2 ))
+
+                if [ $FLIP -eq 0 ]
+
+                then
+                        echo " Head come "
+                else
+                        echo " Tail come "
+                fi
